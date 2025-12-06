32f6a693e0a63facfa3d1954fe78f3d92c27f2e6
Juraj Kollár <mostypc7@gmail.com>
2025-12-06 15:51:14 +0100

    feat: all input boxes done, ready for making the actual installing

--- src-gui/main.py
+++ src-gui/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import subprocess
 import os
 import gi
@@ -10,13 +9,16 @@ debug = True
 
 class Installer(Gtk.Window):
     def __init__(self):
+        self.summary_box = None
         self.selected_locale = None
         self.step = 1
         super().__init__()
         self.set_default_size(800, 600)
 
         header = Gtk.HeaderBar()
-        header.set_show_close_button(True)
+        header.set_show_close_button(False)
+        self.setup_header_buttons(header)
+
         header.set_has_subtitle(False)
         self.set_titlebar(header)
 
@@ -47,7 +49,7 @@ class Installer(Gtk.Window):
         self.description = Gtk.Label()
         self.description.set_markup(
             "<span size='medium'>Hello! We are glad you are installing Redrose Linux. "
-            "This installer will guide you on installing Redrose Linux.</span>"
+            "This app will guide you on the process.</span>"
         )
         self.main_box.pack_start(self.description, False, False, 0)
 
@@ -63,6 +65,10 @@ class Installer(Gtk.Window):
         self.step += 1
         if self.step > 3:
             self.main_box.remove(self.combo)
+        if self.step == 7:
+            self.main_box.remove(self.input)
+        if self.step == 9:
+            self.main_box.remove(self.summary_box)
 
         if self.step == 2:
             self.main_title.set_markup("<span size='xx-large' weight='bold'>Choose your language</span>")
@@ -87,6 +93,7 @@ class Installer(Gtk.Window):
             for locale in self.get_keymaps():
                 self.combo.append_text(locale)
         elif self.step == 4:
+            self.selected_keymap = self.combo.get_active_text()
             self.main_title.set_markup("<span size='xx-large' weight='bold'>What username do you want to use?</span>")
             self.description.set_text(
                 "You will set your password in the next step."
@@ -94,6 +101,61 @@ class Installer(Gtk.Window):
             self.input = Gtk.Entry()
             self.main_box.pack_start(self.input, False, False, 0)
             self.main_box.reorder_child(self.input, 3)
+            self.main_box.show_all()
+        elif self.step == 5:
+            self.username = self.input.get_text()
+            self.main_title.set_markup("<span size='xx-large' weight='bold'>Choose a password</span>")
+            self.description.set_text("Enter the user password.")
+            self.input.set_text("")
+            self.input.set_visibility(False)
+        elif self.step == 6:
+            self.user_password = self.input.get_text()
+            self.main_title.set_markup("<span size='xx-large' weight='bold'>Set the root password</span>")
+            self.description.set_text("Enter the root password.")
+            self.input.set_text("")
+            self.input.set_visibility(False)
+        elif self.step == 7:
+            self.combo = Gtk.ComboBoxText()
+            for drive in self.get_drives():
+                self.combo.append_text(drive)
+            self.combo.set_active(0)
+            self.main_title.set_markup("<span size='xx-large' weight='bold'>Select the target drive</span>")
+            self.description.set_text("Select the drive where Redrose Linux will be installed.")
+            self.main_box.pack_start(self.combo, False, False, 0)
+            self.main_box.reorder_child(self.combo, 3)
+            self.main_box.show_all()
+        elif self.step == 8:
+            self.target_drive = self.combo.get_active_text()
+            self.root_password = self.input.get_text()
+            self.input.set_visibility(True)
+            self.input.set_text("")
+
+            self.main_title.set_markup("<span size='xx-large' weight='bold'>Summary</span>")
+            self.description.set_text(
+                "Please review your selections before proceeding with the installation."
+            )
+
+            self.summary_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+
+            # Helper function to create a row with stock icon + label
+            def summary_row(stock_icon_name, text):
+                row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+                icon = Gtk.Image.new_from_icon_name(stock_icon_name, Gtk.IconSize.MENU)
+                label = Gtk.Label(label=text)
+                label.set_xalign(0)
+                row.pack_start(icon, False, False, 0)
+                row.pack_start(label, True, True, 0)
+                return row
+
+            # Add summary rows with stock icons
+            self.summary_box.pack_start(summary_row("preferences-desktop-locale", f"Locale: {self.selected_locale}"), False, False, 0)
+            self.summary_box.pack_start(summary_row("preferences-desktop-keyboard", f"Keyboard: {self.selected_keymap}"), False, False, 0)
+            self.summary_box.pack_start(summary_row("system-users", f"Username: {self.username} | Password: {'*' * len(self.user_password)}"), False, False, 0)
+            self.summary_box.pack_start(summary_row("preferences-desktop-user-password", f"Root Password: {'*' * len(self.root_password)}"), False, False, 0)
+            self.summary_box.pack_start(summary_row("drive-harddisk", f"Target Drive: {self.target_drive}"), False, False, 0)
+
+            self.main_box.pack_start(self.summary_box, False, False, 0)
+            self.main_box.reorder_child(self.summary_box, 3)
             self.main_box.show_all()
         else:
             self.main_title.set_markup("<span size='xx-large' weight='bold'>Installation was finished.</span>")
@@ -106,7 +168,11 @@ class Installer(Gtk.Window):
             # Create a box for radio buttons
             radio_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
             self.radio1 = Gtk.RadioButton.new_with_label_from_widget(None, "Reboot")
+            self.radio1.set_valign(Gtk.Align.CENTER)
+            self.radio1.set_halign(Gtk.Align.CENTER)
             self.radio2 = Gtk.RadioButton.new_with_label_from_widget(self.radio1, "Keep Testing")
+            self.radio2.set_valign(Gtk.Align.CENTER)
+            self.radio2.set_halign(Gtk.Align.CENTER)
             radio_box.pack_start(self.radio1, False, False, 0)
             radio_box.pack_start(self.radio2, False, False, 0)
 
@@ -132,6 +198,51 @@ class Installer(Gtk.Window):
             self.destroy()
         else:
             print("No option selected")
+
+    def load_icon(self, filename):
+        try:
+            pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename, width=24, height=24, preserve_aspect_ratio=True)
+            return Gtk.Image.new_from_pixbuf(pixbuf)
+        except Exception as e:
+            print(f"Could not load icon {filename}:", e)
+            return Gtk.Image()  # Empty image fallback
+
+    def setup_header_buttons(self, header):
+        theme = "dark" if self.is_dark_theme() else "light"
+        # Close
+        btn_close = Gtk.Button()
+        btn_close.add(self.load_icon(f"close-{theme}mode.png"))
+        btn_close.connect("clicked", lambda w: self.destroy())
+        header.pack_end(btn_close)
+        # Maximize
+        btn_max = Gtk.Button()
+        btn_max.add(self.load_icon(f"maximize-{theme}mode.png"))
+        btn_max.connect("clicked", lambda w: self.maximize() if not self.is_maximized() else self.unmaximize())
+        header.pack_end(btn_max)
+        # Minimize
+        btn_min = Gtk.Button()
+        btn_min.add(self.load_icon(f"minimize-{theme}mode.png"))
+        btn_min.connect("clicked", lambda w: self.iconify())
+        header.pack_end(btn_min)
+
+    def is_dark_theme(self):
+        # Create a temporary widget (not shown)
+        templabel = Gtk.Label()
+        style_context = templabel.get_style_context()
+        bg_color = style_context.get_background_color(Gtk.StateFlags.NORMAL)
+        r, g, b = bg_color.red, bg_color.green, bg_color.blue
+        luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+        del templabel # Because 1 byte of memory is too much to waste :D
+        return luminance < 0.5
+    
+    def get_drives(self):
+        out = subprocess.check_output(["lsblk", "-dn", "-o", "NAME,SIZE,TYPE"], text=True)
+        drives = []
+        for line in out.splitlines():
+            parts = line.split()
+            if parts[2] == "disk":
+                drives.append(f"{parts[0]} ({parts[1]})")
+        return drives
 
     def get_locales(self):
         out = subprocess.check_output(["locale", "-a"], text=True)
